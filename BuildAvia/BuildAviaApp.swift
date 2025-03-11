58c49dad7458b6b68b08983d62fa780667498794
nwent-dev <nwent@proton.me>
2025-03-11 16:18:51 +0300

    Initial Commit

---
+++ BuildAvia/BuildAviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildAviaApp.swift
+//  BuildAvia
+//
+//  Created by Александр Калашников on 11.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BuildAviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
